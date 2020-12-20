1a24e16a6c526d2dca6f0f9a54eeb099f13825ef
Bruno Melo <bmelo@protonmail.com>
2020-12-20 16:49:10 -0300

    consumidor pronto

---
+++ consumer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"os"
+	"github.com/go-pg/pg/v10"
+	//"github.com/go-pg/pg/v10/orm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "eu"
+	dbname   = "postgres"
+  )
+
+func main() {
+	db := pg.Connect(&pg.Options{
+		Addr: ":5432",
+		User: "postgres",
+		Password: "eu",
+		Database: "postgres",
+	})
+	defer db.Close()
+	ctx := context.Background()
+
+	if err := db.Ping(ctx); err != nil {
+		panic(err)
+	}
+	db.Exec("UPDATE t10.ativacao SET opcode="+os.Args[2]+" WHERE id="+os.Args[1]+";")
+}
